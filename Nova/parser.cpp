8948f4643f8683bf1a4e2c29b596f50f9bdbdf75
Delta17x <17sreekarb@gmail.com>
2022-02-07 22:35:05 -0500

    b

--- Nova/parser.cpp
+++ Nova/parser.cpp
@@ -5,6 +5,8 @@
 std::unordered_map<std::string, double> varNumbers;
 std::unordered_map<std::string, std::string> varStrings;
 std::unordered_map<std::string, bool> varBools;
+std::string currentLine;
+size_t currentLineNumber;
 
 void remove_leading_whitespace(std::string& src) {
 	size_t i = 0;
@@ -25,7 +27,7 @@ std::vector<std::string> parse1(const std::string& src) {
 	size_t last = 0;
 	for (size_t i = 0; i < src.size(); i++) {
 		if (src[i] == ';') {
-			parsed.push_back(src.substr(last, i - last)); // remove semicolon and put on new line
+			parsed.push_back(src.substr(last, i - last)); // remove semicolon and put on new currentLine
 			last = i + 1; 
 			// remove whitespace that was after semicolon
 			remove_leading_whitespace(parsed[parsed.size() - 1]);
@@ -35,24 +37,37 @@ std::vector<std::string> parse1(const std::string& src) {
 }
 */
 
-std::string checkForVariableCreation(std::string line) {
-	if (splice(line, 0, 4) == "num ") {
-		size_t equalSignLocation = line.find('=', 4);
-		std::string varName = splice(line, 4, equalSignLocation - 1);
-		varNumbers[varName] = stoi(splice(line, equalSignLocation + 2, line.size()));
+std::string checkForVariableCreation() {
+	if (splice(currentLine, 0, 4) == "num ") {
+		size_t equalSignLocation = currentLine.find('=', 4);
+		std::string varName = splice(currentLine, 4, equalSignLocation - 1);
+		varNumbers[varName] = stoi(splice(currentLine, equalSignLocation + 2, currentLine.size()));
 	}
-	else if (splice(line, 0, 7) == "string ") {
-		size_t equalSignLocation = line.find('=', 7);
-		std::string varName = splice(line, 7, equalSignLocation - 1);
-		varStrings[varName] = splice(line, equalSignLocation + 3, line.size() - 1);
+	else if (splice(currentLine, 0, 7) == "string ") {
+		size_t equalSignLocation = currentLine.find('=', 7);
+		std::string varName = splice(currentLine, 7, equalSignLocation - 1);
+		std::string spliced = splice(currentLine, equalSignLocation + 3, currentLine.size() - 1);
+		if (spliced[0] != '\"' || *spliced.end() != '\"') {
+			return std::string("Unidentified keyword \"" + spliced + "\" found on line " + std::to_string(currentLineNumber) + ".");
+		}
+		varStrings[varName] = spliced;
+	}
+	else if (splice(currentLine, 0, 5) == "bool ") {
+		size_t equalSignLocation = currentLine.find('=', 5);
+		std::string varName = splice(currentLine, 5, equalSignLocation - 1);
+		std::string spliced = splice(currentLine, equalSignLocation + 2, currentLine.size());
+		if (spliced != "false" && spliced != "0" && spliced != "true" && spliced != "1") {
+			return std::string("Unidentified keyword \"" + spliced + "\" found on line " + std::to_string(currentLineNumber) + ".");
+		}
+		varBools[varName] = (spliced == "true" || spliced == "1");
 	}
 	return "";
 }
 
-std::string checkForFunctionCall(std::string line) {
-	if (splice(line, 0, 6) == "print(") {
-		size_t closeParenLocation = line.find(')', 6);
-		std::string fullMessage = splice(line, 6, closeParenLocation);
+std::string checkForFunctionCall() {
+	if (splice(currentLine, 0, 6) == "print(") {
+		size_t closeParenLocation = currentLine.find(')', 6);
+		std::string fullMessage = splice(currentLine, 6, closeParenLocation);
 
 		if (fullMessage[0] == '\"' && fullMessage[fullMessage.size() - 1] == '\"') { // Calling with literal
 			std::cout << splice(fullMessage, 1, fullMessage.size() - 1);
@@ -73,7 +88,7 @@ std::string checkForFunctionCall(std::string line) {
 						std::cout << iteratorForBools->second;
 					}
 					else
-						return std::string("Function parameter \"" + fullMessage + "\" not found.");
+						return std::string("Unidentified function argument \"" + fullMessage + "\" found on line " + std::to_string(currentLineNumber) + ".");
 				}
 					
 			}
@@ -82,17 +97,23 @@ std::string checkForFunctionCall(std::string line) {
 	return "";
 }
 
-std::string parseLine(std::string line) {
-	return checkForVariableCreation(line) + "; " + checkForFunctionCall(line);
+std::pair<std::string, std::string> parseLine() {
+	return std::pair<std::string, std::string>(checkForVariableCreation(), checkForFunctionCall());
 }
 
 int main() {
+	std::cout << std::boolalpha;
 	std::ifstream fileToRead("debug.txt");
-	std::string currentLine;
+
 	while (std::getline(fileToRead, currentLine)) {
-		auto retMessage = parseLine(currentLine);
-		if (retMessage != "; ") {
-			std::cout << retMessage << std::endl;
+		currentLineNumber++;
+		auto retMessages = parseLine();
+		if (!retMessages.first.empty()) {
+			std::cout << "Error: " << retMessages.first << std::endl;
+			break;
+		}
+		if (!retMessages.second.empty()) {
+			std::cout << "Error: " << retMessages.second << std::endl;
 			break;
 		}
 	}
--- Nova/parser.hpp
+++ Nova/parser.hpp
@@ -6,11 +6,8 @@
 
 typedef std::size_t size_t;
 
-// Returns an array with each line of code on its own element.
-std::vector<std::string> parse1(const std::string& src);
-
-std::string checkForVariableCreation(std::string line);
+std::string checkForVariableCreation();
 std::string checkForFunctionCreation(std::string line);
-std::string checkForFunctionCall(std::string line);
+std::string checkForFunctionCall();
 
-std::string parseLine(std::string line);
+std::pair<std::string, std::string> parseLine(std::string line);
